a0de38135df7b44edad7d865ba3d6447fc6e4029
Miles Johnson <mileswjohnson@gmail.com>
2024-01-30 16:09:55 -0800

    Add npm.

---
+++ crates/package-managers/src/npm.rs
@@ -0,0 +1,75 @@
+use package_lock_json_parser::parse;
+
+pub use package_lock_json_parser::PackageLockJsonError;
+use rustc_hash::FxHashMap;
+
+pub struct PackageLockDependency {
+    pub name: String,
+    pub version: Option<String>,
+    pub integrity: Option<String>,
+    pub dependencies: FxHashMap<String, String>,
+}
+
+pub struct PackageLockJson;
+
+impl PackageLockJson {
+    pub fn parse<T: AsRef<str>>(
+        content: T,
+    ) -> Result<Vec<PackageLockDependency>, PackageLockJsonError> {
+        let data = parse(content.as_ref())?;
+
+        // v1
+        if let Some(entries) = data.dependencies {
+            return Ok(entries
+                .into_iter()
+                .map(|(name, entry)| PackageLockDependency {
+                    name,
+                    version: if entry.version.is_empty() {
+                        None
+                    } else {
+                        Some(entry.version.to_owned())
+                    },
+                    integrity: entry.integrity,
+                    dependencies: FxHashMap::from_iter(
+                        entry
+                            .dependencies
+                            .unwrap_or_default()
+                            .into_iter()
+                            .map(|(k, v)| (k, v.version)),
+                    ),
+                })
+                .collect());
+        }
+
+        // v1
+        if let Some(entries) = data.packages {
+            return Ok(entries
+                .into_iter()
+                .map(|(name, entry)| {
+                    let mut dependencies = FxHashMap::default();
+
+                    if let Some(prod_deps) = entry.dependencies {
+                        dependencies.extend(prod_deps);
+                    }
+
+                    if let Some(dev_deps) = entry.dev_dependencies {
+                        dependencies.extend(dev_deps);
+                    }
+
+                    PackageLockDependency {
+                        name: entry.name.unwrap_or(name),
+                        version: if entry.version.is_empty() {
+                            None
+                        } else {
+                            Some(entry.version.to_owned())
+                        },
+                        integrity: entry.integrity,
+                        dependencies,
+                    }
+                })
+                .collect());
+        }
+
+        Ok(vec![])
+    }
+}
