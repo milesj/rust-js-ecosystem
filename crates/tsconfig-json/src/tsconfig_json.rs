8c1000e8103b3c45c28290b70ad6187f5c90cca2
Miles Johnson <mileswjohnson@gmail.com>
2024-02-12 16:33:00 -0800

    Add resolve extends.

--- crates/tsconfig-json/src/tsconfig_json.rs
+++ crates/tsconfig-json/src/tsconfig_json.rs
@@ -1,14 +1,15 @@
 use crate::compiler_options::CompilerOptions;
 use crate::path_types::PathOrGlob;
 use serde::Deserialize;
-use std::collections::BTreeMap;
-use std::path::PathBuf;
+use std::collections::{BTreeMap, VecDeque};
+use std::path::{Path, PathBuf};
+use std::{fs, io};
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 #[serde(untagged)]
 pub enum TsConfigExtends {
-    String(PathBuf),
-    Array(Vec<PathBuf>),
+    Single(PathBuf),
+    Multiple(Vec<PathBuf>),
 }
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq)]
@@ -32,6 +33,45 @@ pub struct TsConfigJson {
 
     #[serde(flatten)]
     pub other_fields: BTreeMap<String, serde_json::Value>,
+}
+
+pub struct ResolvedTsConfigChain {
+    pub path: PathBuf,
+    pub config: TsConfigJson,
+}
+
+impl TsConfigJson {
+    pub fn resolve_extends_chain<T: AsRef<Path>>(
+        path: T,
+    ) -> io::Result<Vec<ResolvedTsConfigChain>> {
+        let mut chain = vec![];
+        let mut queue = VecDeque::new();
+        queue.push_back(path.as_ref().to_owned());
+
+        while let Some(path) = queue.pop_front() {
+            let config = fs::read(&path)?;
+            let config: TsConfigJson = serde_json::from_slice(&config)?;
+
+            if let Some(extends) = &config.extends {
+                let parent_dir = path.parent().unwrap();
+
+                for extends_path in match extends {
+                    TsConfigExtends::Single(other) => vec![other],
+                    TsConfigExtends::Multiple(others) => others.iter().collect(),
+                } {
+                    queue.push_back(if extends_path.extension().is_none() {
+                        parent_dir.join(extends_path).join("tsconfig.json")
+                    } else {
+                        parent_dir.join(extends_path)
+                    })
+                }
+            }
+
+            chain.push(ResolvedTsConfigChain { path, config })
+        }
+
+        Ok(chain)
+    }
 }
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq)]
