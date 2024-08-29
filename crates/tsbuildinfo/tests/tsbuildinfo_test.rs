1bd4f155ea23bc62d39a0d20be996e400d221122
Miles Johnson <mileswjohnson@gmail.com>
2024-08-28 22:04:52 -0700

    Test serialize.

--- crates/tsbuildinfo/tests/tsbuildinfo_test.rs
+++ crates/tsbuildinfo/tests/tsbuildinfo_test.rs
@@ -7,14 +7,16 @@ mod v5_x {
     use super::*;
 
     #[test]
-    fn deserializes() {
-        let path = locate_fixture("files").join("5.x.json");
-        let info: TsBuildInfo = serde_json::from_slice(&fs::read(path).unwrap()).unwrap();
+    fn deserializes_and_serializes() {
+        let data = fs::read(locate_fixture("files").join("5.x.json")).unwrap();
+        let info: TsBuildInfo = serde_json::from_slice(&data).unwrap();
 
         assert_eq!(info.version, "5.4.5");
         assert_eq!(
             info.program.latest_changed_dts_file,
             Some(PathBuf::from("./cjs/index.d.ts"))
         );
+
+        let _ = serde_json::to_string(&info).unwrap();
     }
 }
